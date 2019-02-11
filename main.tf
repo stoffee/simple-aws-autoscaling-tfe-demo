f7902073b768392293b33cd81294f7bc7a91994e
Chris Dunlap <chris@roadkill.org>
2019-02-11 14:50:58 -0800

    created main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "us-west-2"
+}
+
+variable "aws_access_key" {
+  description = "access key"
+}
+
+variable "aws_secret_key" {
+  description = "secret key"
+}
+
+resource "aws_launch_template" "demo-lt" {
+  name_prefix   = "demo-"
+  image_id      = "ami-076b01046426fd1c5"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "demo-ag" {
+  availability_zones = ["us-west-2a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+
+  launch_template {
+    id      = "${aws_launch_template.demo-lt.id}"
+    version = "$$Latest"
+  }
+}
